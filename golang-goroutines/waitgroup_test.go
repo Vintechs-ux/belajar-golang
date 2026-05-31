ebdd1319d0a4890874639b2448ebe93e57c30758
Vintech <radityaramadhanimalang@gmail.com>
2026-05-31 14:51:08 +0700

    race_condition_Test: implementasi Waitgroup dan mutex untuk mengatasi race condition di goroutines

--- golang-goroutines/waitgroup_test.go
+++ golang-goroutines/waitgroup_test.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func RunAsynchronous(group *sync.WaitGroup) {
@@ -25,4 +27,78 @@ func TestWaitGroup(t *testing.T) {
 
 	group.Wait()
 	fmt.Println("Complete")
+}
+
+var counting = 0
+
+func OnlyOnce() {
+	counting++
+}
+
+func TestOnlyOnce(t *testing.T) {
+	var once sync.Once
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+
+	for i := 0; i < 100; i++ {
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mutex.Lock()
+			once.Do(OnlyOnce)
+			mutex.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(counting)
+}
+
+var counter = 0
+
+func Counter() {
+	counter++
+}
+
+func TestRaceWaitGroup(t *testing.T) {
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+
+	for i := 0; i < 100; i++ {
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mutex.Lock()
+			Counter()
+			mutex.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(counter)
+}
+
+func TestRaceWaitGroupLoop(t *testing.T) {
+	for putaran := 1; putaran <= 1000; putaran++ {
+		t.Run(fmt.Sprintf("Putaran-%d", putaran), func(t *testing.T) {
+			counter := 0
+			var wg sync.WaitGroup
+			var mutex sync.Mutex
+
+			for i := 0; i < 100; i++ {
+				mutex.Lock()
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					counter++
+					mutex.Unlock()
+				}()
+			}
+			wg.Wait()
+
+			assert.Equal(t, 100, counter, "program seharusnya tidak mengalami race condition")
+		})
+	}
 }
