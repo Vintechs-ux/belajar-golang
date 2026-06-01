cecb486daf32ee6d326fa4d3c1d954868736ace4
Vintech <suryabarumalang@gmail.com>
2026-06-01 07:22:28 +0700

    pool_test: implementasi pool dasar

---
+++ golang-goroutines/pool_test.go
@@ -0,0 +1,35 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestPool(t *testing.T) {
+	var pool sync.Pool
+	var wg sync.WaitGroup
+
+	pool.Put("Diki")
+	pool.Put("Surya")
+	pool.Put("Saputra")
+
+	data1 := pool.Get()
+	fmt.Println("diluar go: ", data1)
+	data2 := pool.Get()
+	fmt.Println("diluar go: ", data2)
+	pool.Put(data1)
+	pool.Put(data2)
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			data := pool.Get()
+			fmt.Println(data)
+			pool.Put(data)
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Proses selesai")
+}
