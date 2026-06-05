02a40b7d55734689928f1632b3e68c52e0278de6
Vintech <suryabarumalang@gmail.com>
2026-06-05 10:33:53 +0700

    func: TestShapeInterface

--- golang-practice/data_race_test.go
+++ golang-practice/data_race_test.go
@@ -26,3 +26,62 @@ func TestDataRace(t *testing.T) {
 	wg.Wait()
 	fmt.Println(idr_balance)
 }
+
+type Human struct {
+	Name string
+}
+
+func (h Human) Speak() {
+	fmt.Println("Hello")
+}
+
+func Speak() {
+	fmt.Println("Hello")
+}
+
+type Speaker interface {
+	Speak()
+}
+
+func Talk(s Speaker) {
+	s.Speak()
+}
+
+func TestFunc(t *testing.T) {
+	siregar := Human{"Siregar"}
+
+	Talk(siregar)
+}
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * 3.14
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func PrintArea(s Shape) {
+	fmt.Println(s.Area())
+}
+
+func TestShapeInterface(t *testing.T) {
+	meja_belajar := Rectangle{5, 10}
+	meja_bundar := Circle{5}
+
+	PrintArea(meja_belajar)
+	PrintArea(meja_bundar)
+}
